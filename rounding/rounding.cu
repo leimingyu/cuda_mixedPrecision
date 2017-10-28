3fc23ec3daed7fe6010580ef8ef9483de381d74f
Leiming Yu <leimingyu830@gmail.com>
2017-10-28 17:31:41 -0400

    rounding

---
+++ rounding/rounding.cu
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <cuda_fp16.h>
+#include <assert.h>
+#include "fp16_conversion.h"   // host function for half conversion
+
+inline
+cudaError_t checkCuda(cudaError_t result)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+#endif
+  return result;
+}
+
+__global__
+void myTest(float *x, half *y)
+{
+    float v=2.51f;
+    //float v=2.5f;
+    //float v=-2.5f;
+    //float v=-2.51f;
+    x[0] = floorf(v);
+    y[0] = hfloor(__float2half(v));
+}
+
+int main(int argc, char** argv) {
+
+  int devid =0;
+
+  if (argc != 2) {
+      fprintf(stderr, "Specify device to use only. (./program devid)\n");
+      exit(1);
+  }else {
+  	devid = atoi(argv[1]);
+  }
+
+  cudaSetDevice(devid);
+
+  cudaDeviceProp prop;                                                    
+  cudaGetDeviceProperties(&prop, devid);                                 
+  printf("device %d : %s\n", devid, prop.name);
+
+  float *x;
+  checkCuda(cudaMallocManaged(&x, sizeof(float)));
+
+  half *y;
+  checkCuda(cudaMallocManaged(&y, sizeof(half)));
+  
+  myTest<<<1, 1>>>(x, y);
+
+  checkCuda(cudaDeviceSynchronize());
+  
+  // check results
+  printf("%f\n", x[0]);
+  printf("%f\n", half_to_float(y[0]));
+
+
+
+  return 0;
+}
