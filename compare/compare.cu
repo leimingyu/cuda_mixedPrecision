f9b5c80d5ddbcdf870635b92d60917de41d80ac2
leiming yu <leimingyu830@gmail.com>
2017-11-14 23:43:16 -0500

    abs

--- compare/compare.cu
+++ compare/compare.cu
@@ -22,25 +22,23 @@ cudaError_t checkCuda(cudaError_t result)
 __global__
 void myTest(int n, float a, const float *x, half *y)
 {
-	//int gid = threadIdx.x + blockDim.x * blockIdx.x;
+/*
+	half a_half = __float2half(a);
+	y[0] = __hadd(a_half, __hge(a_half, bool2half(__float2half(0.f)))); 
+*/
 
-	//half a_half = __float2half(a);
-	//y[gid] = __hadd(a_half, __hge(a_half, bool2half(__float2half(0.f)))); 
+
 
 	half2 zero2 = __floats2half2_rn(0.f, 0.f);
 	half2 minusone = __floats2half2_rn(-1.f, -1.f);
-	half2 v2    = __floats2half2_rn(2.15f, -3.14f);
-	half2 r2;
-
-	printf("v2:  %f %f\n", __low2half(v2),  __high2half(v2));
-
-	// abs
-	half2 cmp2 = __hlt2(v2, zero2);
-	printf("(compare) %f %f\n", __low2half(cmp2),  __high2half(cmp2));
-
-	r2 = __hmul2(__hadd2(__hadd2(cmp2, cmp2), minusone), v2);
-	
-	printf("%f %f\n", __low2half(r2),  __high2half(r2));
+	half2 v2    = __floats2half2_rn(2.1f, -3.1f);
+	printf("v2:  %f %f\n", __half2float(__low2half(v2)),  __half2float(__high2half(v2)));
+	// compare 
+	half2 cmp2 = __hgt2(v2, zero2);
+	printf("(compare >0.f) %f %f\n", __half2float(__low2half(cmp2)),  __half2float(__high2half(cmp2)));
+	// abs 
+	half2 r2 = __hmul2(__hadd2(__hadd2(cmp2, cmp2), minusone), v2);
+	printf("(abs)%f %f\n", __half2float(__low2half(r2)),  __half2float(__high2half(r2)));
 }
 
 int main(int argc, char** argv) {
@@ -55,7 +53,7 @@ int main(int argc, char** argv) {
   const int n = 1;
 
   const float a = 1.f; 
-  printf("a = %f\n", a);
+  //printf("a = %f\n", a);
 
   float *x;
   checkCuda(cudaMallocManaged(&x, n * sizeof(float)));
